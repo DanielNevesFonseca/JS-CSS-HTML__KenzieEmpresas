4e0a60b4a5ec180d6643d5dc4b936d2187b9c8d2
Daniel Neves Fonseca - Web Fullstack Developer <daniel.neves.fonseca.ti@gmail.com>
2023-07-06 16:51:07 -0300

    add base structure of Kenzie Empresas

---
+++ src/scripts/toast.js
@@ -0,0 +1,17 @@
+export const colorSuccess = '#36B37E';
+export const colorError = '#FF5630';
+
+export function toast(message, color){
+  Toastify({
+    text: message,
+    duration: 2000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      backgroundColor: color,
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+}
